e93d9d123f0d6f4ffffecaf8eee6361b97042242
André Ferreira <aferreira_ti@hotmail.com>
2020-06-22 16:04:20 -0300

    Acrescentada uma msg de bom dia

--- projeto_teste/Program.cs
+++ projeto_teste/Program.cs
@@ -7,6 +7,7 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!");
+            Console.WriteLine("Bom dia!");
             Console.ReadLine();
         }
     }
