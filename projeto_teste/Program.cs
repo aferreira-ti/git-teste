bb580f2bc42bb66d87ba81c3cbc7f9ed95f68a47
André Ferreira <aferreira_ti@hotmail.com>
2020-06-22 16:01:42 -0300

    Projeto iniciado, mostrando a msg mostrar mundo

---
+++ projeto_teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+            Console.ReadLine();
+        }
+    }
+}
